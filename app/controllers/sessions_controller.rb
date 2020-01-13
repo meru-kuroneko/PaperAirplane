ade7248fc88359d9f4950c46ad36ce1df424a837
meru-kuroneko <canopy.4160+github@gmail.com>
2020-01-13 12:58:44 +0900

    change 取得した情報をsessionとcookieに保存

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,48 @@
 class SessionsController < ApplicationController
   def create
     user_data = request.env['omniauth.auth']
-    if session[:user_ids].nil?
-      session[:user_ids] = []
-    end
-    if session[:user_info].nil?
-      session[:user_info] = []
-    end
-    session[:user_ids].push(user_data[:uid])
-    session[:user_info].push(user_data[:info])
-    cookies[:credentials] = {value: {id: user_data[:uid], value: user_data[:credentials]}, secure: true, httpOnly: true}
+    add_user_ids(user_data[:uid])
+    add_user_info(user_data[:extra][:raw_info])
+    add_credentials(user_data[:uid], user_data[:credentials])
     redirect_to root_path, notice: 'ログインしました'
+  end
+
+  private
+
+  def add_user_ids(uid)
+    ids = session[:user_ids]
+    if ids.nil?
+      ids = []
+    end
+    if ids.exclude?(uid)
+      ids.push(uid)
+    end
+    session[:user_info] = ids
+  end
+
+  def add_user_info(user_info)
+    info = session[:user_info]
+    if info.nil?
+      info = []
+    end
+    if info.exclude?(user_info)
+      info.push(user_info)
+    end
+    session[:user_info] = info
+  end
+
+  def add_credentials(uid, credentials)
+    cookie = cookies[:credentials]
+    val = {"token" => credentials[:token], "secret" => credentials[:secret]}
+    json_item = JSON.generate({uid => {"token" => credentials[:token], "secret" => credentials[:secret]}})
+
+    if cookie.nil?
+      cookie = {value: json_item, secure: true, httpOnly: true}
+    elsif cookie[uid].nil?
+      cookie[uid] = val
+    else
+      cookie.store(uid, val)
+    end
+    cookies[:credentials] = cookie
   end
 end
